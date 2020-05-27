a1440f4c78412fd5e55d02851338dce106213bc7
nardo-leo <jora.karuzo@gmail.com>
2020-05-27 13:36:04 +0300

    Change pics in selected layers from first directory in project and auto push to render

--- _img-multiplier.jsx
+++ _img-multiplier.jsx
@@ -1,28 +1,8 @@
-// Get text line by line from text file to array
-function readText() {
-
-    var textFile = File.openDialog('Choose Text File');
-    var textLines = [];
-
-    if (textFile != null) {
-        textFile.open('r');
-        while(!textFile.eof) {
-            textLines[textLines.length] = textFile.readln();
-        }
-        textFile.close();
-    }
-
-    if (!textLines) {
-        alert('ERROR Reading file');
-        return null;
-    }
-
-    return textLines;
-
-}
-
 // Get the active composition
 var activeComp = app.project.activeItem;
+
+// Get pics folder #1 - FIX add possibility to choose
+var picsFolder = app.project.item(1);
 
 // Check if we really have a composition
 if (activeComp != null && (activeComp instanceof CompItem)) {
@@ -30,15 +10,14 @@ if (activeComp != null && (activeComp instanceof CompItem)) {
     // Change output location
     var outputFolder = Folder.selectDialog('Choose Save Location', '');
 
-    // Get all the selected layers
+    // Get all the selected layers to change
     var selectedLayers = activeComp.selectedLayers;
 
-    // Manipulate with source images by cycle through textLines array
-    textLines = readText();
-    for (var i = 0; i < textLines.length; i++) {
+    // Manipulate with source images by cycle through pics directory in Ae
+    for (var i = 1; i <= picsFolder.numItems; i++) {
         for (var j = 0; j < selectedLayers.length; j++) {
 
-            var sourceImg = new ImportOptions(File(textLines[i])); // FIX - doesn't work because of type of import
+            var sourceImg = picsFolder.item(i);
             selectedLayers[j].replaceSource(sourceImg, true);
 
             var saveFolder = new Folder(outputFolder);
@@ -50,7 +29,7 @@ if (activeComp != null && (activeComp instanceof CompItem)) {
             var item = app.project.renderQueue.items.add(activeComp);
             var outputModule = item.outputModule(1);
             outputModule.applyTemplate('Lossless'); // Get possibility to choose it
-            outputModule.file = File(String(saveFolder) + '/' + activeComp.name + '#' + (i + 1));
+            outputModule.file = File(String(saveFolder) + '/' + activeComp.name + '#' + (i));
 
             // Render
             app.project.renderQueue.render();
