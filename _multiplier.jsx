94d7a927a7178d4fd49f78c6293ffac212b56206
nardo-leo <jora.karuzo@gmail.com>
2020-06-19 21:27:51 +0300

    Read dataset and consistently substitute values to comp's text layer

--- _multiplier.jsx
+++ _multiplier.jsx
@@ -1,7 +1,5 @@
-// Get text line by line from text file to array
-function readText() {
+function readDatasetColumns(textFile) {
 
-    var textFile = File.openDialog('Choose Dataset');
     var textLines = [];
 
     if (textFile != null) {
@@ -18,7 +16,7 @@ function readText() {
                     columns.push([cells[j]]); // fill column's header
                 } else {
                     columns[j].push(cells[j]); // fill columns
-                }
+               }
             }
         }
         textFile.close();
@@ -29,45 +27,82 @@ function readText() {
         return null;
     }
 
-    return textLines;
+    return columns;
 
 }
 
-// Get the active composition
-var activeComp = app.project.activeItem;
+function readDatasetLines(textFile) {
 
-// Check if we really have a composition
-if (activeComp != null && (activeComp instanceof CompItem)){
+    var textLines = [];
 
-    // Change output location
-    var outputFolder = Folder.selectDialog('Choose Save Location', '');
+    if (textFile != null) {
+        textFile.open('r');
+        while(!textFile.eof) {
+            textLines[textLines.length] = textFile.readln(); // lines
+        }
 
-    // Get all the selected layers
-    var selectedLayers = activeComp.selectedLayers;
+    }
 
-    // Manipulate with source text by cycle through textLines array
-    textLines = readText();
-    for (var i = 0; i < textLines.length; i++) {
-        for (var j = 0; j < selectedLayers.length; j++) {
+    return textLines;
+}
 
-            selectedLayers[j].sourceText.setValue(textLines[i]);
 
-            var saveFolder = new Folder(outputFolder);
-                if (! saveFolder.exist) {
-                    saveFolder.create();
+function pushToRender(activeComp, saveFolder, c, l) {
+
+    var item = app.project.renderQueue.items.add(activeComp);
+    var outputModule = item.outputModule(1);
+    outputModule.applyTemplate('Lossless'); // TODO Get possibility to choose it
+    // TODO change places
+    outputModule.file = File(String(saveFolder) + '/' + activeComp.name + '#' + l + '-' + (c + 1));
+
+    // Render
+    app.project.renderQueue.render();
+
+    // Sending to AME instead
+    // app.project.renderQueue.queueInAME(true);
+
+}
+
+
+function main() {
+    // Get the active composition
+    var activeComp = app.project.activeItem;
+
+    // Choose dataset file
+    var textFile = File.openDialog('Choose Dataset');
+
+    // Check if we really have a composition
+    if (activeComp != null && (activeComp instanceof CompItem)) {
+
+        // Change output location
+        var outputFolder = Folder.selectDialog('Choose Save Location', '');
+
+        // Get all the selected layers
+        var selectedLayers = activeComp.selectedLayers; // TODO mark layers to distinguish layers
+
+        // Manipulate with source text by cycle through columns array
+        var columns = readDatasetColumns(textFile);
+        var lines = readDatasetLines(textFile);
+
+        // avoid line #0 because it's header
+        for (var l = 1; l < lines.length; l++) {
+            for (var c = 0; c < columns.length; c++) {
+                for (var s = 0; s < selectedLayers.length; s++) {
+
+                    selectedLayers[s].sourceText.setValue(columns[c][l]);
+
+                    var saveFolder = new Folder(outputFolder);
+                        if (! saveFolder.exist) {
+                            saveFolder.create();
+                        }
+
+                    // Push composition to render queue
+                    pushToRender(activeComp, saveFolder, c, l);
                 }
-
-            // Add composition to render queue
-            var item = app.project.renderQueue.items.add(activeComp);
-            var outputModule = item.outputModule(1);
-            outputModule.applyTemplate('Lossless'); // TODO Get possibility to choose it
-            outputModule.file = File(String(saveFolder) + '/' + activeComp.name + '#' + (i + 1));
-
-            // Render
-            app.project.renderQueue.render();
-
-            // Sending to AME instead
-            // app.project.renderQueue.queueInAME(true);
+            }
         }
     }
 }
+
+
+main();
