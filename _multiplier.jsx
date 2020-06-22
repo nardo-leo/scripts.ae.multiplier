e86de62d6da0c88ac9f9d64b4a11be1f2de81a44
nardo-leo <jora.karuzo@gmail.com>
2020-06-22 20:25:31 +0300

    Add searching for img-dir inside project, start working with image substitution

--- _multiplier.jsx
+++ _multiplier.jsx
@@ -52,7 +52,6 @@ function pushToRender(activeComp, saveFolder, c, l) {
     var item = app.project.renderQueue.items.add(activeComp);
     var outputModule = item.outputModule(1);
     outputModule.applyTemplate('Lossless'); // TODO Get possibility to choose it
-    // TODO change places
     outputModule.file = File(String(saveFolder) + '/' + activeComp.name + '#' + l + '-' + (c + 1));
 
     // Render
@@ -84,12 +83,41 @@ function main() {
         var columns = readDatasetColumns(textFile);
         var lines = readDatasetLines(textFile);
 
-        // avoid line #0 because it's header
+        // Get img folder
+        var imgFolder = '';
+        for (var i = 1; i < app.project.numItems; i++) {
+            if (app.project.item(i).name == 'img' && app.project.item(i) instanceof FolderItem) {
+                imgFolder = app.project.item(i);
+            }
+        }
+        // Check if don't find img folder
+        if (imgFolder == '') {
+            alert("Can't find folder 'img'");
+            return;
+        }
+
+        // Change content
+        // Avoid line #0 because it's header
         for (var l = 1; l < lines.length; l++) {
             for (var c = 0; c < columns.length; c++) {
                 for (var s = 0; s < selectedLayers.length; s++) {
 
-                    selectedLayers[s].sourceText.setValue(columns[c][l]);
+                    var layerName = selectedLayers[s].name;
+                    var layerStartsWith = layerName.charAt(0);
+
+                    if (layerStartsWith == '^') { // change text
+                        selectedLayers[s].sourceText.setValue(columns[c][l]);
+                    }
+                    else if (layerStartsWith == '#') { // change image
+                        for (var i = 1; i <= imgFolder.numItems; i++) {
+                            if (imgFolder.item(i).name == columns[c][l]) { //FIXME can't bypass inside
+                                selectedLayers[s].replaceSource(imgFolder.item(i), true);
+                            }
+                        }
+                    } else {
+                        alert('You need to mark layer type (^ - text, # - image)');
+                        return;
+                    }
 
                     var saveFolder = new Folder(outputFolder);
                         if (! saveFolder.exist) {
@@ -97,7 +125,7 @@ function main() {
                         }
 
                     // Push composition to render queue
-                    pushToRender(activeComp, saveFolder, c, l);
+                    // pushToRender(activeComp, saveFolder, c, l);
                 }
             }
         }
